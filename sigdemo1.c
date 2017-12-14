efbeb97c3c71335104f32462dd697a4837ee08fd
rambler-song <thdwodud9402@naver.com>
2017-12-14 21:15:19 +0900

    song2

---
+++ sigdemo1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <signal.h>
+
+int main(){
+	void f(int);
+	int i;
+	signal(SIGINT, SIG_IGN);
+	printf("you can not stop me\n");
+	for(i =0; i<5; i++){
+		printf("Hello\n");
+		sleep(1);
+	}
+}
+void f(int signum){
+	printf("OUCH!\n");
+}
